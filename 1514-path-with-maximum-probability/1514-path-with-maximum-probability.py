c6b0442716663bcd7f3db6caa7e9813267de6512
zaidsaeed <zsaee060@uottawa.ca>
2022-03-24 20:40:11 -0400

    Time: 868 ms (66.30%), Space: 26.1 MB (66.30%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -2,27 +2,37 @@ import heapq
 class Solution:
     
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        graph = defaultdict(list) ## Initialise graph
-        visited = set() ## Initialise visited set
+        def setUp(n, edges, succProb, start):
+            graph = {}
+            matrix = []
+            heap = []
+            for i in range(n):
+                if (i == start):
+                    matrix.append(1)
+                    heap.append((-1, i))
+                else:
+                    matrix.append(0)
+                graph[i] = []
+            for edge, weight in zip(edges, succProb):
+                u = edge[0]
+                v = edge[1]
+                graph[u].append((v, weight))
+                graph[v].append((u, weight))
+            heapq.heapify(heap)
+            return graph, matrix, heap
         
-        for idx, (src, dst) in enumerate(edges): ## Traverse through the edges array to update the graph
-            graph[src].append((dst, succProb[idx]))
-            graph[dst].append((src, succProb[idx]))
-            
-        probs = [0]*n ## Initialise a probablity array to indicate maximum success probablity from start node to all the other nodes
-        probs[start] = 1 ## Success probablity to the start node itself is 1
-        
-        pq = [(-1, start)] ## Push the result to maxHeap in (prob, node) format -> Hence prob*=-1 
-        heapify(pq)
-        
-		## Dijkstra's algorithm
-        while pq:
-            _, node = heappop(pq)
-            for nei, currSuccProb in graph[node]:
-                if not nei in visited and probs[node]*currSuccProb > probs[nei]:
-                    probs[nei] = probs[node]*currSuccProb
-                    heappush(pq, (-probs[nei], nei))
+        def dijkstra(graph, matrix, heap, start, end):
+            visited = set()
+            while heap:
+                u = heapq.heappop(heap)[1]
+                for info in graph[u]:
+                    v = info[0]
+                    weight = info[1]
+                    if not v in visited and (matrix[u] * weight > matrix[v]):
+                        matrix[v] = max(matrix[v], matrix[u] * weight)
+                        heapq.heappush(heap, (-1 * matrix[v], v))
+                visited.add(u)
+            return matrix[end]
                 
-            visited.add(node)
-                
-        return probs[end]
+        graph, matrix, heap = setUp(n, edges, succProb, start)
+        return dijkstra(graph, matrix, heap, start, end)
