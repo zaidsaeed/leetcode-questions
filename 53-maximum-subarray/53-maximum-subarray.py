ea7bf0df7057710f329ee6666ae799318f6c450f
zaidsaeed <zsaee060@uottawa.ca>
2022-04-06 00:05:28 -0400

    Time: 1476 ms (7.39%), Space: 28.4 MB (18.23%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,9 +2,11 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         ans = [float('-inf') for i in range(len(nums))]
         ans[0] = nums[0]
+        res = nums[0]
         for i in range(1, len(nums)):
             ans[i] = max(nums[i], ans[i-1] + nums[i])
-        return max(ans)
+            res = max(ans[i], res)
+        return res
     
     # [-2,1,-3,4,-1,2,1,-5,4]
     # [-2,1,-2,4,3,5,6,1,5 ]
