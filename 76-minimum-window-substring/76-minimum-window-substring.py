2d0bd1bf86bf2ff17ba9d08fc752e0e8bc6e1158
zaidsaeed <zsaee060@uottawa.ca>
2022-04-13 01:21:53 -0400

    Time: 619 ms (9.76%), Space: 28 MB (5.07%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,61 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        def getStrMap(string, tMap):
+            charMap = {}
+            for char in string:
+                if tMap and char in tMap:
+                    charMap[char] = charMap.get(char, 0) + 1
+                else:
+                    charMap[char] = charMap.get(char, 0) + 1
+            return charMap
+        
+        def filterS(string, tMap):
+            res = []
+            for i in range(len(string)):
+                char = string[i]
+                if char in tMap:
+                    res.append((char, i))
+            return res
+        
+        def twoMapsEqual(sMap, tMap):
+            if len(sMap.keys()) != len(tMap.keys()):
+                return False
+            for key, val in tMap.items():
+                if key not in sMap:
+                    return False
+                if sMap[key] < val:
+                    return False
+            return True
+        
+        tMap = getStrMap(t, None)
+        filteredS = filterS(s, tMap)
+        sMap = getStrMap(s, tMap)
+        
+        l, r = 0, 0
+        ans = ''
+        windowMap = {}
+        done = False
+        while r < len(filteredS):
+            if not twoMapsEqual(windowMap,tMap):
+                char = filteredS[r][0]
+                windowMap[char] = windowMap.get(char, 0) + 1
+                
+                while l <= r and twoMapsEqual(windowMap,tMap):
+                    newAns = s[filteredS[l][1]:(filteredS[r][1] + 1)]
+                    if ans == '' or len(newAns) < len(ans):
+                        print(newAns)
+                        ans = newAns
+                    windowMap[filteredS[l][0]] = windowMap[filteredS[l][0]] - 1
+                    if windowMap[filteredS[l][0]] == 0:
+                        del windowMap[filteredS[l][0]]
+                    l += 1
+                r += 1
+        print(filteredS)
+        print(l)
+        print(r)
+        return ans
+                
+            
+            
+        
