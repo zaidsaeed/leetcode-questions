bc287770bd3f7c463257465d23cb8f0f411ab865
zaidsaeed <zsaee060@uottawa.ca>
2022-05-12 17:54:49 -0400

    Time: 88 ms (22.68%), Space: 14.6 MB (26.67%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        shorter, longer = None, None
+        if len(s) > len(t):
+            return False
+        elif len(t) > len(s):
+            return False
+        else:
+            shorter = s
+            longer = t
+        
+            def getCharArr(string):
+                charArr = [0 for i in range(26)]
+                for char in string:
+                    index = ord(char) - ord('a')
+                    charArr[index] += 1
+                return charArr
+
+            shorterArr = getCharArr(shorter)
+            longerArr = getCharArr(longer)
+
+            for i in range(len(shorterArr)):
+                if shorterArr[i] != longerArr[i]:
+                    return False
+            return True
