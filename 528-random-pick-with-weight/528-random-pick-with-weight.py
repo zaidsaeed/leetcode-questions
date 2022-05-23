4bbaeff3c3a956220263ddab2a272336e185a78e
zaidsaeed <zsaee060@uottawa.ca>
2022-05-22 21:31:09 -0400

    Time: 530 ms (25.05%), Space: 18.5 MB (79.42%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -7,6 +7,12 @@ class Solution:
     def pickIndex(self) -> int:
         randomNum = random.randint(1, self.prefix[-1])
         return self.findIndex(randomNum)
+        print('--------')
+        print(self.prefix)
+        print(randomNum)
+        print(self.findIndex(randomNum))
+        print(self.pickIndexSol(self.prefix, randomNum))
+        print('--------')
 
     def genPrefix(self, w):
         ans = []
@@ -15,18 +21,30 @@ class Solution:
             rSum += elem
             ans.append(rSum)
         return ans
+    
+    def pickIndexSol(self, ps, target):
+        l, r = 0, len(ps) - 1
+        while l < r:
+            m = (l + r) // 2
+            if target > ps[m]: l = m + 1
+            else: r = m
+        return l
 
     def findIndex(self, target):
         l = 0
         h = len(self.prefix) - 1
-        ans = 0
-        while l < h:
-            m = l + ((h-l) // 2)
-            if self.prefix[m] == target:
-                return m
-            elif self.prefix[m] < target:
-                ans = m
+        ans = h
+        while l <= h:
+            m = l + ((h-l + 1) // 2)
+            if self.prefix[m] < target:
                 l = m + 1
-            elif self.prefix[m] > target:
-                h = m
-        return l
+            elif self.prefix[m] >= target:
+                ans = m
+                h = m - 1
+        return ans
+
+    '''
+    [3, 17, 18, 25]
+     l   m       h
+    '''
+    
