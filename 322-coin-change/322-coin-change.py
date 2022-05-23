501a626317f5e1dbaa327c51387c603eb9439f66
zaidsaeed <zsaee060@uottawa.ca>
2022-05-23 13:18:59 -0400

    Time: 2203 ms (35.43%), Space: 14.5 MB (35.78%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,37 +1,38 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
         
-        def coinChangeRec(coins, amount, cache, count):
-    
-            if amount in cache:
-                return cache[amount]
+        q, dp = [], [float('inf') for i in range(amount+1)]
+        
+        for coin in coins:
+            if coin < amount:
+                dp[coin] = 1
+                q.append(coin)
+            elif coin == amount:
+                return 1
             
-            if amount == 0:
-                return count
-            
-            if amount < 0:
-                return float('inf')
-            
-            ans = float('inf')
+        while q:
+            val = q.pop(0)
+            count = dp[val]
+            # print(val)
+            # print(count)
+            # print('     ')
             for coin in coins:
-                remainder = amount-coin
-                ans = min(ans, coinChangeRec(coins, remainder, cache, count))
-            
-            cache[amount] = ans + 1
-            
-            return cache[amount]
+                if val + coin < amount and dp[val + coin] == float('inf'):
+                    q.append(val + coin)
+                    dp[val+coin] = min(dp[val+coin], count+1)
+                elif val + coin == amount:
+                    return count + 1
         
-        cache = {}
-        res = coinChangeRec(coins, amount, cache, 0)
-        if res == float('inf'):
-            return -1
-        return res
+        return -1
+
 
     '''
         coins = [1,2,5], amount = 11
-                        
-                        11
-                    10   9  6
+        dp = [_, 1, 1, _, _, 1, 2, 2, 3, _, 3]    
+                  
+                       
         
     
     '''
