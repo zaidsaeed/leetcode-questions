e751f4c919b5f07a5e7f1c191c58bcbe309628c7
zaidsaeed <zsaee060@uottawa.ca>
2022-06-14 08:10:27 -0400

    Time: 1528 ms (57.68%), Space: 29.8 MB (31.49%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -4,7 +4,6 @@ class Solution:
         ans = [0] * len(heights)
         for i in range(len(heights)-1, -1, -1):
             curr_height = heights[i]
-            cnt = 0
             while stack and curr_height > stack[-1]:
                 stack.pop()
                 ans[i] += 1 
