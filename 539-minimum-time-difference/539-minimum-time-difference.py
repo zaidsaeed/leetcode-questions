a29c828e8e9256f9c0f86d44ada2a891dc9b5fa2
zaidsaeed <zsaee060@uottawa.ca>
2022-06-21 16:03:19 -0400

    Time: 143 ms (18.89%), Space: 17.4 MB (16.69%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -5,17 +5,61 @@ class Solution:
         for tp in timePoints:
             nums.append(self.convertToNum(tp))
         
-        nums.sort()
+        buckets, over = self.generateBucket(nums)
+        
+        if over:
+            return 0
         
         ans = float('inf')
-        for i in range(1, len(nums)):
-            ans = min(ans, abs(nums[i] - nums[i-1]))
         
-        ans = min(ans, (1440 - nums[-1] + nums[0]))
+        for num in nums:
+            numClosest = self.findClosestTp(buckets, num)
+            ans = min(ans, numClosest)
         
         return ans
+    
+    
+    def findClosestTp(self, buckets, num):
+        #search left 
+        i = num - 1
+        if i == -1:
+            i = 1439
+        found = buckets[i] == 1
+        countLeft = 1
+        while not found:
+            countLeft += 1
+            i -= 1
+            if i == -1:
+                i = 1439
+            found = buckets[i] == 1
+                
+
+        
+        #search right
+        i = num + 1
+        if i == 1440:
+            i = 0
+        found = buckets[i] == 1
+        countRight = 1
+        while not found:
+            countRight += 1
+            i += 1
+            if i == 1440:
+                i = 0
+            found = buckets[i] == 1
+
+        
+        return min(countLeft, countRight)
     
     def convertToNum(self, tp):
         mins = int(tp[3] + tp[4])
         hrs = int(tp[0] + tp[1])
         return (hrs * 60) + mins
+    
+    def generateBucket(self, nums):
+        buckets = [0 for i in range(0, 1440)]
+        for num in nums:
+            if buckets[num] == 1:
+                return None, True
+            buckets[num] += 1
+        return buckets, False
