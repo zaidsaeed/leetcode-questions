531f2f6143a4e51cfeaeeb371afabcfb40fe1b4d
zaidsaeed <zsaee060@uottawa.ca>
2022-06-21 16:28:32 -0400

    Time: 123 ms (34.29%), Space: 17.1 MB (59.42%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -1,20 +1,34 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        nums = []
+        nums, buckets = [], [0 for i in range(0, 1440)]
         
         for tp in timePoints:
-            nums.append(self.convertToNum(tp))
+            num = self.convertToNum(tp)
+            nums.append(num)
+            if buckets[num] == 1:
+                return 0
+            buckets[num] += 1
         
-        buckets, over = self.generateBucket(nums)
+        i = 0
+        while buckets[i] != 1:
+            i += 1
         
-        if over:
-            return 0
+        first = i
         
-        ans = float('inf')
+        i = 1439
+        while buckets[i] != 1:
+            i -= 1
         
-        for num in nums:
-            numClosest = self.findClosestTp(buckets, num)
-            ans = min(ans, numClosest)
+        last = i
+        
+
+        ans = (1440 - last) + first
+        
+        for i in range(first + 1, len(buckets)):
+            num = buckets[i]
+            if num:
+                ans = min(ans, i - first)
+                first = i
         
         return ans
     
@@ -32,34 +46,11 @@ class Solution:
             if i == -1:
                 i = 1439
             found = buckets[i] == 1
-                
 
-        
-        #search right
-        i = num + 1
-        if i == 1440:
-            i = 0
-        found = buckets[i] == 1
-        countRight = 1
-        while not found:
-            countRight += 1
-            i += 1
-            if i == 1440:
-                i = 0
-            found = buckets[i] == 1
-
-        
-        return min(countLeft, countRight)
+        return countLeft
     
     def convertToNum(self, tp):
         mins = int(tp[3] + tp[4])
         hrs = int(tp[0] + tp[1])
         return (hrs * 60) + mins
     
-    def generateBucket(self, nums):
-        buckets = [0 for i in range(0, 1440)]
-        for num in nums:
-            if buckets[num] == 1:
-                return None, True
-            buckets[num] += 1
-        return buckets, False
