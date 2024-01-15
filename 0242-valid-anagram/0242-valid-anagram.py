29a3e4e53e7d385d7b98887a0d373692250b28e7
zaidsaeed <zsaee060@uottawa.ca>
2024-01-15 06:15:47 -0800

    Time: 57 ms (39.51%), Space: 17.7 MB (24.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sCounter = Counter(s)
+        tCounter = Counter(t)
+        for i in range(len(s)):
+            sChar = s[i]
+            if not sChar in tCounter:
+                return False
+            else:
+                tCounter[sChar] -= 1
+                if tCounter[sChar] == 0:
+                    del tCounter[sChar]
+        
+        return True
