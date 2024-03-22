2215a7be8bbec541376f33c5033851be299fd228
zaidsaeed <zsaee060@uottawa.ca>
2024-03-23 00:46:43 +0300

    Time: 233 ms (5.11%), Space: 18.5 MB (18.98%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,39 @@
+class Solution:
+    
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = self.createAdjList(numCourses, prerequisites)
+        classWithNoPrereqs = self.getNextClass(graph)
+        print(classWithNoPrereqs)
+        while classWithNoPrereqs != None:
+            self.removeClassFromGraph(classWithNoPrereqs, graph)
+            classWithNoPrereqs = self.getNextClass(graph)
+            print(classWithNoPrereqs)
+
+        print(graph)
+        return len(graph.keys()) == 0
+    
+    def removeClassFromGraph(self, classToRemove, graph):
+        for aclass, preqs in graph.items():
+            if classToRemove in preqs:
+                preqs.remove(classToRemove)
+                
+        del graph[classToRemove]
+    
+    def getNextClass(self, graph):
+        for aclass, preqs in graph.items():
+            if len(preqs) == 0:
+                return aclass
+        return None
+            
+    def createAdjList(self, numCourses, prerequisites):
+        graph = {}
+        
+        for i in range(numCourses):
+            graph[i] = set()
+        
+        for relation in prerequisites:
+            course, prereq = relation
+            graph[course].add(prereq)
+        
+        return graph
+            
