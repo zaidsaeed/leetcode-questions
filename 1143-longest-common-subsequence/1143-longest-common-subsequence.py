a683856f4963278b142355376423f384c00e3505
zaidsaeed <zsaee060@uottawa.ca>
2024-05-20 21:06:35 +0300

    Time: 1434 ms (5.32%), Space: 184.3 MB (18.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n, m = len(text1), len(text2)
-        
-        matrix = [[0 for i in range(m+1)] for j in range(n+1)]
-        
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if text1[i-1] == text2[j-1]:
-                    matrix[i][j] = 1 + matrix[i-1][j-1]
-                else:
-                    matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
-        
-        return matrix[-1][-1]
+        memo = {}
+        return self.lcs(text1, text2, 0, 0, memo)
+    
+    def lcs(self, text1, text2, idx1, idx2, memo):
+        if idx1 < len(text1) and idx2 < len(text2):
+            if (idx1, idx2) in memo:
+                return memo[(idx1, idx2)]
+            if text1[idx1] == text2[idx2]:
+                memo[(idx1, idx2)] = 1 + self.lcs(text1, text2, idx1+1, idx2+1, memo)
+                return memo[(idx1, idx2)]
+            branch1 = self.lcs(text1, text2, idx1, idx2+1, memo)
+            branch2 = self.lcs(text1, text2, idx1+1, idx2, memo)
+            memo[(idx1, idx2)] = max(branch1, branch2)
+            return memo[(idx1, idx2)]
+        return 0
+
+'''
+     a b c d e
+   a 1 1 1 1 1
+   c 1 1 2 2 2 
+   e 1 1 2 2 3
+
+'''
